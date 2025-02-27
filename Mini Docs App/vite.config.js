c787a4bc94cbcbd2ed502a4e7d0ad94d0a387fc7
Aryan Patel <aryanpatel6215@gmail.com>
2025-02-27 16:18:33 +0530

    commit

--- Mini Docs App/vite.config.js
+++ Mini Docs App/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:'/Mini-Docs-App'
+  base:"/Mini-Docs-App/",
 })
